ed97e6e750c2102158526b1a023031ea1b699402
dl-watson <dlarkinwatson@gmail.com>
2021-02-04 15:35:19 -0800

    all base requirement elements on page

--- src/components/pages/UserInterface.jsx
+++ src/components/pages/UserInterface.jsx
@@ -1,8 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "./styles/UserInterface.css";
 
 const UserInterface = (props) => {
-  return <div></div>;
+  return (
+    <main className={styles.UserInterface}>
+      <div className={styles.URLInput}>
+        <input />
+        <button>Go</button>
+      </div>
+      <div className={styles.radioButtons}>
+        <form>
+          <button type="radio" name="get">
+            GET
+          </button>
+          <button type="radio" name="put">
+            PUT
+          </button>
+          <button type="radio" name="post">
+            POST
+          </button>
+          <button type="radio" name="delete">
+            DELETE
+          </button>
+        </form>
+      </div>
+      <div className={styles.JSONInput}>
+        <span
+          className={styles.textArea}
+          placeholder="JSON"
+          role="textbox"
+          contentEditable
+        ></span>
+      </div>
+    </main>
+  );
 };
 
 UserInterface.propTypes = {};

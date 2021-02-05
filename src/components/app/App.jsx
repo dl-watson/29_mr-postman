633f4d351a879c32d9819aec760d1c171d27b738
dl-watson <dlarkinwatson@gmail.com>
2021-02-04 17:05:38 -0800

    moved around Router, turned App back into a class component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,33 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "../headers/Header";
 import Home from "../../containers/Home";
 import History from "../pages/History";
 import Help from "../pages/Help";
 
-const App = () => {
-  return (
-    <div className="main">
-      <Router>
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={(routerProps) => <Home {...routerProps} />}
-          />
-          <Route
-            path="/history"
-            exact
-            render={(routerProps) => <History {...routerProps} />}
-          />
-          <Route
-            path="/help"
-            exact
-            render={(routerProps) => <Help {...routerProps} />}
-          />
-        </Switch>
-      </Router>
-    </div>
-  );
-};
-
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div className="main">
+        <Router>
+          <Header />
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={(routerProps) => <Home {...routerProps} />}
+            />
+            <Route
+              path="/history"
+              exact
+              render={(routerProps) => <History {...routerProps} />}
+            />
+            <Route
+              path="/help"
+              exact
+              render={(routerProps) => <Help {...routerProps} />}
+            />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import UserInterface from "../components/pages/UserInterface";
-import Header from "../components/headers/Header";
 import styles from "./styles/Home.css";
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div className={styles.Home}>
-        <Header />
-        <UserInterface />
-      </div>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <div className={styles.Home}>
+      <UserInterface />
+    </div>
+  );
+};
+
+export default Home;
